7c5291148ece71a1b4192380effd2d5c7f4a8789
Rideema14 <rideemasinghjbp@gmail.com>
2025-08-02 20:19:03 +0530

    code

--- src/Day5.jsx
+++ src/Day5.jsx
@@ -1,15 +1,50 @@
 import React, { useEffect, useState } from 'react'
-
+import './Day5.css'
 const Day5 = () => {
-    let [count,SetCount]=useState(0);
+
+    let [input ,SetInput]=useState([]);
     useEffect(()=>{
-        console.log("hello");
+        fetch('https://jsonplaceholder.typicode.com/todos').
+        then((res)=>{
+            return res.json()
+
+        }).then((data)=>{
+            SetInput(data)
+            console.log(data);
+            
+        })
         
     },[])
+
+    function d(index){
+       let nerArr=   input.filter((a)=>{
+            return a.id!=index
+
+        })
+        SetInput(nerArr)
+        // console.log(nerArr, "heheheheh");
+    }
+    function dataSave(data) {
+      localStorage.setItem("key",JSON.stringify(data))
+      
+    }
+    
   return (
     <div>
-        <button onClick={()=>{SetCount(count+1)}}>count{count}</button>
-
+        {/* <button onClick={()=>SetCount(count+1)}> count {count}
+        </button> */}
+        <div id='parent_Card'>  
+        {
+            input.map((a,index)=>{
+                return(
+                // <div id='card'  onClick={()=>d(a.id)} >
+                 <div id='card'>
+                <h1>{a.id}</h1>
+                <button onClick={()=>dataSave(a)}>add</button>
+                </div>)
+            })
+        }
+         </div>
     </div>
   )
 }
